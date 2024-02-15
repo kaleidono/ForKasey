c36e47e865c64696dd2c0e8e547ede33516ba835
kaleidono <155097708+kaleidono@users.noreply.github.com>
2024-02-15 21:52:39 +0800

    Update api_commands.ts

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,18 +1,1 @@
-// // List of commands that require API calls
 
-// import { getQuote } from '../api';
-// import { getWeather } from '../api';
-
-// export const quote = async (args: string[]): Promise<string> => {
-  // const data = await getQuote();
-  // return data.quote;
-// };
-
-// export const weather = async (args: string[]): Promise<string> => {
-  // const city = args.join('+');
-  // if (!city) {
-    // return 'Usage: weather [city]. Example: weather casablanca';
-  // }
-  // const weather = await getWeather(city);
-  // return weather;
-// };
